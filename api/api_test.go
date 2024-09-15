962a0a579ce87a9f86063bb6bccbf60b7fedfb46
Christer Knorborg <christerknorborg@hotmail.com>
2024-09-15 08:22:12 +0200

    added additional tests for handlers and routes

--- api/api_test.go
+++ api/api_test.go
@@ -12,7 +12,6 @@ import (
 // operand1 = 10, operand2 = 5. The response should be 15.
 func TestAddHandler(t *testing.T) {
 
-	// "GET", "/add?operand1="10"&operand2="5", nil
 	request := httptest.NewRequest("GET", "/add?operand1=10&operand2=5", nil)
 	responseRecorder := httptest.NewRecorder()
 
@@ -23,11 +22,58 @@ func TestAddHandler(t *testing.T) {
 		t.Fatalf("expected status 200, got %d", responseRecorder.Code)
 	}
 
+	// Check header
+	if responseRecorder.Header().Get("Content-Type") != "application/json" {
+		t.Fatalf("expected Content-Type application/json, got %s", responseRecorder.Header().Get("Content-Type"))
+	}
+
 	// Check the result directly from the response body
 	expected := `{"result":15}`
 	actual := strings.TrimSpace(responseRecorder.Body.String()) // remove whitespace from JSON
 	if actual != expected {
 		t.Fatalf("expected %v, got %v", expected, actual)
+	}
+}
+
+// TestAddHandlerWithInvalidOperands checks how the addition handler handles invalid operands:
+// operand1 = invalid, operand2 = 5. Response should be 400 status code.
+func TestAddHandlerWithInvalidOperands(t *testing.T) {
+	request := httptest.NewRequest("GET", "/add?operand1=invalid&operand2=5", nil)
+	responseRecorder := httptest.NewRecorder()
+
+	api.AddHandler(responseRecorder, request)
+
+	// Check for bad status code
+	if responseRecorder.Code != http.StatusBadRequest {
+		t.Fatalf("expected status 400, got %d", responseRecorder.Code)
+	}
+
+	// Check error message is returned
+	expectedError := "invalid operands\n"
+	actualError := responseRecorder.Body.String()
+	if actualError != expectedError {
+		t.Fatalf("expected %v, got %v", expectedError, actualError)
+	}
+}
+
+// TestAddHandlerWithMissingOperands checks the addition handler with missing operands.
+// operand1 = 10, operand2 is missing. Response should be 400 status code.
+func TestAddHandlerWithMissingOperands(t *testing.T) {
+	request := httptest.NewRequest("GET", "/add?operand1=10", nil)
+	responseRecorder := httptest.NewRecorder()
+
+	api.AddHandler(responseRecorder, request)
+
+	// Check for the bad request status code
+	if responseRecorder.Code != http.StatusBadRequest {
+		t.Fatalf("expected status 400, got %d", responseRecorder.Code)
+	}
+
+	// Check error message is returned
+	expectedError := "invalid operands\n"
+	actualError := responseRecorder.Body.String()
+	if actualError != expectedError {
+		t.Fatalf("expected %v, got %v", expectedError, actualError)
 	}
 }
 
@@ -45,11 +91,59 @@ func TestSubtractHandler(t *testing.T) {
 		t.Fatalf("expected status 200, got %d", responseRecorder.Code)
 	}
 
+	// Check header
+	if responseRecorder.Header().Get("Content-Type") != "application/json" {
+		t.Fatalf("expected Content-Type application/json, got %s", responseRecorder.Header().Get("Content-Type"))
+	}
+
 	// Check the result directly from the response body
 	expected := `{"result":5}`
 	actual := strings.TrimSpace(responseRecorder.Body.String()) // remove whitespace from JSON
 	if actual != expected {
 		t.Fatalf("expected %v, got %v", expected, actual)
+	}
+}
+
+// TestSubtractHandlerWithInvalidOperands checks the subtraction handler with invalid operands:
+// operand1 = 10, operand2 = invalid. Response should be 400 status code.
+func TestSubtractHandlerWithInvalidOperands(t *testing.T) {
+
+	request := httptest.NewRequest("GET", "/subtract?operand1=10&operand2=invalid", nil)
+	responseRecorder := httptest.NewRecorder()
+
+	api.SubtractHandler(responseRecorder, request)
+
+	// Check bad status code
+	if responseRecorder.Code != http.StatusBadRequest {
+		t.Fatalf("expected status 400, got %d", responseRecorder.Code)
+	}
+
+	// Check error message is returned
+	expectedError := "invalid operands\n"
+	actualError := responseRecorder.Body.String()
+	if actualError != expectedError {
+		t.Fatalf("expected %v, got %v", expectedError, actualError)
+	}
+}
+
+// TestSubtractHandlerWithMissingOperands checks the subtract handler with missing operands.
+// operand1 = 10, operand2 is missing. Response should be 400 status code.
+func TestSubtractHandlerWithMissingOperands(t *testing.T) {
+	request := httptest.NewRequest("GET", "/subtract?operand1=10", nil)
+	responseRecorder := httptest.NewRecorder()
+
+	api.SubtractHandler(responseRecorder, request)
+
+	// Check for the bad request status code
+	if responseRecorder.Code != http.StatusBadRequest {
+		t.Fatalf("expected status 400, got %d", responseRecorder.Code)
+	}
+
+	// Check error message is returned
+	expectedError := "invalid operands\n"
+	actualError := responseRecorder.Body.String()
+	if actualError != expectedError {
+		t.Fatalf("expected %v, got %v", expectedError, actualError)
 	}
 }
 
@@ -67,11 +161,59 @@ func TestMultiplyHandler(t *testing.T) {
 		t.Fatalf("expected status 200, got %d", responseRecorder.Code)
 	}
 
+	// Check header
+	if responseRecorder.Header().Get("Content-Type") != "application/json" {
+		t.Fatalf("expected Content-Type application/json, got %s", responseRecorder.Header().Get("Content-Type"))
+	}
+
 	// Check the result directly from the response body
 	expected := `{"result":50}`
 	actual := strings.TrimSpace(responseRecorder.Body.String()) // remove whitespace from JSON
 	if actual != expected {
 		t.Fatalf("expected %v, got %v", expected, actual)
+	}
+}
+
+// TestMultiplyHandlerWithInvalidOperands checks the multiply handler with invalid operands:
+// operand1 = invalid, operand2 = 45. Response should be 400 status code.
+func TestMultiplyHandlerWithInvalidOperands(t *testing.T) {
+
+	request := httptest.NewRequest("GET", "/multiply?operand1=invalid&operand2=45", nil)
+	responseRecorder := httptest.NewRecorder()
+
+	api.MultiplyHandler(responseRecorder, request)
+
+	// Check bad status code
+	if responseRecorder.Code != http.StatusBadRequest {
+		t.Fatalf("expected status 400, got %d", responseRecorder.Code)
+	}
+
+	// Check error message is returned
+	expectedError := "invalid operands\n"
+	actualError := responseRecorder.Body.String()
+	if actualError != expectedError {
+		t.Fatalf("expected %v, got %v", expectedError, actualError)
+	}
+}
+
+// TestMultiplyHandlerWithMissingOperands checks the multiply handler with missing operands.
+// operand1 is missing, operand2 = 10. Response should be 400 status code.
+func TestMultiplyHandlerWithMissingOperands(t *testing.T) {
+	request := httptest.NewRequest("GET", "/multiply?operand2=10", nil)
+	responseRecorder := httptest.NewRecorder()
+
+	api.MultiplyHandler(responseRecorder, request)
+
+	// Check for the bad request status code
+	if responseRecorder.Code != http.StatusBadRequest {
+		t.Fatalf("expected status 400, got %d", responseRecorder.Code)
+	}
+
+	// Check error message is returned
+	expectedError := "invalid operands\n"
+	actualError := responseRecorder.Body.String()
+	if actualError != expectedError {
+		t.Fatalf("expected %v, got %v", expectedError, actualError)
 	}
 }
 
@@ -89,11 +231,81 @@ func TestDivideHandler(t *testing.T) {
 		t.Fatalf("expected status 200, got %d", responseRecorder.Code)
 	}
 
+	// Check header
+	if responseRecorder.Header().Get("Content-Type") != "application/json" {
+		t.Fatalf("expected Content-Type application/json, got %s", responseRecorder.Header().Get("Content-Type"))
+	}
+
 	// Check the result directly from the response body
 	expected := `{"result":2}`
 	actual := strings.TrimSpace(responseRecorder.Body.String()) // remove whitespace from JSON
 	if actual != expected {
 		t.Fatalf("expected %v, got %v", expected, actual)
+	}
+}
+
+// TestDivideHandlerWithInvalidOperands checks the divide handler with invalid operands:
+// operand1 = invalid, operand2 = 45. Response should be 400 status code.
+func TestDivideHandlerWithInvalidOperands(t *testing.T) {
+
+	request := httptest.NewRequest("GET", "/subtract?operand1=invalid&operand2=45", nil)
+	responseRecorder := httptest.NewRecorder()
+
+	api.DivideHandler(responseRecorder, request)
+
+	// Check bad status code
+	if responseRecorder.Code != http.StatusBadRequest {
+		t.Fatalf("expected status 400, got %d", responseRecorder.Code)
+	}
+
+	// Check error message is returned
+	expectedError := "invalid operands\n"
+	actualError := responseRecorder.Body.String()
+	if actualError != expectedError {
+		t.Fatalf("expected %v, got %v", expectedError, actualError)
+	}
+}
+
+// TestDivideHandlerWithMissingOperands checks the divide handler with missing operands.
+// operand1 is missing, operand2 = 10. Response should be 400 status code.
+func TestDivideHandlerWithMissingOperands(t *testing.T) {
+	request := httptest.NewRequest("GET", "/divide?operand2=10", nil)
+	responseRecorder := httptest.NewRecorder()
+
+	api.DivideHandler(responseRecorder, request)
+
+	// Check for the bad request status code
+	if responseRecorder.Code != http.StatusBadRequest {
+		t.Fatalf("expected status 400, got %d", responseRecorder.Code)
+	}
+
+	// Check error message is returned
+	expectedError := "invalid operands\n"
+	actualError := responseRecorder.Body.String()
+	if actualError != expectedError {
+		t.Fatalf("expected %v, got %v", expectedError, actualError)
+	}
+}
+
+// TestDivideHandlerWithInvalidOperands checks the divide handler with division by zero:
+// operand1 = 45, operand2 = 0. Response should be 400 status code.
+func TestDivideHandlerWithDivisionByZero(t *testing.T) {
+
+	request := httptest.NewRequest("GET", "/multiply?operand1=10&operand2=0", nil)
+	responseRecorder := httptest.NewRecorder()
+
+	api.DivideHandler(responseRecorder, request)
+
+	// Check status code
+	if responseRecorder.Code != http.StatusBadRequest {
+		t.Fatalf("expected status 400, got %d", responseRecorder.Code)
+	}
+
+	// Check error message is returned
+	expectedError := "cannot divide by zero\n"
+	actualError := responseRecorder.Body.String()
+	if actualError != expectedError {
+		t.Fatalf("expected %v, got %v", expectedError, actualError)
 	}
 }
 
@@ -111,11 +323,81 @@ func TestModuloHandler(t *testing.T) {
 		t.Fatalf("expected status 200, got %d", responseRecorder.Code)
 	}
 
+	// Check header
+	if responseRecorder.Header().Get("Content-Type") != "application/json" {
+		t.Fatalf("expected Content-Type application/json, got %s", responseRecorder.Header().Get("Content-Type"))
+	}
+
 	// Check the result directly from the response body
 	expected := `{"result":1}`
 	actual := strings.TrimSpace(responseRecorder.Body.String()) // remove whitespace from JSON
 	if actual != expected {
 		t.Fatalf("expected %v, got %v", expected, actual)
+	}
+}
+
+// TestModuloHandlerWithInvalidOperands checks the modulo handler with invalid operands:
+// operand1 = 45, operand2 = invalid. Response should be 400 status code.
+func TestModuloHandlerWithInvalidOperands(t *testing.T) {
+
+	request := httptest.NewRequest("GET", "/modulo?operand1=45&operand2=invalid", nil)
+	responseRecorder := httptest.NewRecorder()
+
+	api.ModuloHandler(responseRecorder, request)
+
+	// Check bad status code
+	if responseRecorder.Code != http.StatusBadRequest {
+		t.Fatalf("expected status 400, got %d", responseRecorder.Code)
+	}
+
+	// Check error message is returned
+	expectedError := "invalid operands\n"
+	actualError := responseRecorder.Body.String()
+	if actualError != expectedError {
+		t.Fatalf("expected %v, got %v", expectedError, actualError)
+	}
+}
+
+// TestModuloHandlerWithMissingOperands checks the modulo handler with missing operands.
+// operand1 = 45, operand2 is missing. Response should be 400 status code.
+func TestModuloHandlerWithMissingOperands(t *testing.T) {
+	request := httptest.NewRequest("GET", "/modulo?operand1=45", nil)
+	responseRecorder := httptest.NewRecorder()
+
+	api.ModuloHandler(responseRecorder, request)
+
+	// Check for bad status code
+	if responseRecorder.Code != http.StatusBadRequest {
+		t.Fatalf("expected status 400, got %d", responseRecorder.Code)
+	}
+
+	// Check error message is returned
+	expectedError := "invalid operands\n"
+	actualError := responseRecorder.Body.String()
+	if actualError != expectedError {
+		t.Fatalf("expected %v, got %v", expectedError, actualError)
+	}
+}
+
+// TestModuloHandlerWithDivisionByZero checks the modulo handler with division by zero:
+// operand1 = 45, operand2 = 0. Response should be 400 status code.
+func TestModuloHandlerWithModuloByZero(t *testing.T) {
+
+	request := httptest.NewRequest("GET", "/modulo?operand1=45&operand2=0", nil)
+	responseRecorder := httptest.NewRecorder()
+
+	api.ModuloHandler(responseRecorder, request)
+
+	// Check status code
+	if responseRecorder.Code != http.StatusBadRequest {
+		t.Fatalf("expected status 400, got %d", responseRecorder.Code)
+	}
+
+	// Check error message is returned
+	expectedError := "cannot modulo by zero\n"
+	actualError := responseRecorder.Body.String()
+	if actualError != expectedError {
+		t.Fatalf("expected %v, got %v", expectedError, actualError)
 	}
 }
 
@@ -133,10 +415,36 @@ func TestPowerHandler(t *testing.T) {
 		t.Fatalf("expected status 200, got %d", responseRecorder.Code)
 	}
 
+	// Check header
+	if responseRecorder.Header().Get("Content-Type") != "application/json" {
+		t.Fatalf("expected Content-Type application/json, got %s", responseRecorder.Header().Get("Content-Type"))
+	}
+
 	// Check the result directly from the response body
 	expected := `{"result":8}`
 	actual := strings.TrimSpace(responseRecorder.Body.String()) // remove whitespace from JSON
 	if actual != expected {
 		t.Fatalf("expected %v, got %v", expected, actual)
+	}
+}
+
+// TestPowerHandlerWithInvalidOperands checks the power handler with invalid operands:
+// operand1 = invalid, operand2 = 3. Response should be 400 status code.
+func TestPowerHandlerWithInvalidOperands(t *testing.T) {
+	request := httptest.NewRequest("GET", "/power?operand1=invalid&operand2=3", nil)
+	responseRecorder := httptest.NewRecorder()
+
+	api.PowerHandler(responseRecorder, request)
+
+	// Check for bad status code
+	if responseRecorder.Code != http.StatusBadRequest {
+		t.Fatalf("expected status 400, got %d", responseRecorder.Code)
+	}
+
+	// Check error message is returned
+	expectedError := "invalid operands\n"
+	actualError := responseRecorder.Body.String()
+	if actualError != expectedError {
+		t.Fatalf("expected %v, got %v", expectedError, actualError)
 	}
 }
